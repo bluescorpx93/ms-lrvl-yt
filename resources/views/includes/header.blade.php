0c538873b728b92ae1f5059064665df4cfb37e79
Toufiq <burningraven06@gmail.com>
2018-05-15 14:51:05 +0600

    create views, use include require

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,37 @@
+<header>
+
+
+	{{-- <nav>
+    <div class="nav-wrapper">
+      <a href="#" class="brand-logo">Logo</a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        
+      </ul>
+    </div>
+  </nav> --}}
+
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <!-- Brand and toggle get grouped for better mobile display -->
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+	        <span class="sr-only">Toggle navigation</span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="#">Brand</a>
+	    </div>
+
+	    <!-- Collect the nav links, forms, and other content for toggling -->
+	    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+	      <ul class="nav navbar-nav">
+	        
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+
+
+
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,39 +1,47 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.master')
 
-        <title>Laravel SocialNetwork App</title>
-        
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-                        <a href="{{ route('register') }}">Register</a>
-                    @endauth
-                </div>
-            @endif
+@section('title')
+Welcome
+@endsection
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+@section('div_content')
+	<div class='row'>
+		<div class='col-md-6'>
+			<h3> Sign Up</h3>
+			<form action="#" method="post">
+				<div class='form-group'>
+					<label for="email"> Email</label>
+					<input type='text' class='form-control' name='email' id='email'>
+				</div>
+				<div class='form-group'>
+					<label for="first_name"> First Name</label>
+					<input type='text' class='form-control' name='first_name' id='first_name'>
+				</div>
+				<div class='form-group'>
+					<label for="password"> Password</label>
+					<input type='text' class='form-control' name='password' id='password'>
+				</div>
+				<div class='form-group'>
+					<button type='submit' class='btn btn-primary'>Submit</button>
+				</div>
+			</form>
+		</div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+		<div class='col-md-6'>
+			<h3> Sign In</h3>
+			<form action="#" method="post">
+				<div class='form-group'>
+					<label for="email"> Email</label>
+					<input type='text' class='form-control' name='email' id='email'>
+				</div>
+				<div class='form-group'>
+					<label for="password"> Password</label>
+					<input type='text' class='form-control' name='password' id='password'>
+				</div>
+				<div class='form-group'>
+					<button type='submit' class='btn btn-primary'>Submit</button>
+				</div>
+			</form>
+		</div>
+	</div>
+@endsection
