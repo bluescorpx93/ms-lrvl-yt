5c156b3299e219809596de693e64cd421e36196b
Toufiq <burningraven06@gmail.com>
2018-05-15 15:36:58 +0600

    create User Controller, create User Model

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,30 @@
+<?php 
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+
+class UserController extends Controller{
+
+	public function postSignUp(Request $req){
+		$email = $req['email'];
+		$first_name = $req['first_name'];
+		$password = bcrypt($req['password']);
+
+		$user = new User();
+		$user->email = $email;
+		$user->first_name = $first_name;
+		$user->password = $password;
+
+		$user->save();
+
+		return redirect()-back();
+	}
+
+
+	public function postSignIn(Request $req){
+
+	}
+}
